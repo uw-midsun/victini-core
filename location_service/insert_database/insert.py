c69e86fa7cc3328ebfb7122d57a3b061124893dc
Ryan Lam <53351930+ryan-lam@users.noreply.github.com>
2023-06-05 12:07:03 -0400

    update requirements.txt and script

--- location_service/insert_database/insert.py
+++ location_service/insert_database/insert.py
@@ -10,22 +10,26 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-engine = create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}")
+# Put path to csv file here
+df = pd.read_csv("./location_service/insert_database/sample_coordinates.csv")
 
-df = pd.read_csv("mock_database/bulk_inserts/coordinates/coordinates.csv")
 
-with engine.connect() as connection:
-    try:
-        for index, row in df.iterrows():
-            stmt = text(
-                "INSERT INTO location_service.coordinates (coordinates, latitude, longitude) VALUES (ST_GeomFromText(:coordinates, 4326), :latitude, :longitude)"
-            )
-            params = {
-                "coordinates": f"POINT({row.latitude} {row.longitude})",
-                "latitude": row.latitude,
-                "longitude": row.longitude,
-            }
-            connection.execute(stmt, params)
-        connection.commit()
-    except Exception as e:
-        print(f"An error occurred: {e}")
+if __name__ == "__main__":
+    engine = create_engine(
+        f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    )
+    with engine.connect() as connection:
+        try:
+            for index, row in df.iterrows():
+                stmt = text(
+                    "INSERT INTO location_service.coordinates (coordinates, latitude, longitude) VALUES (ST_GeomFromText(:coordinates, 4326), :latitude, :longitude)"
+                )
+                params = {
+                    "coordinates": f"POINT({row.latitude} {row.longitude})",
+                    "latitude": row.latitude,
+                    "longitude": row.longitude,
+                }
+                connection.execute(stmt, params)
+            connection.commit()
+        except Exception as e:
+            print(f"An error occurred: {e}")
