c97be40ae605aad00fd551e87e124a1622292459
Ryan Lam <53351930+ryan-lam@users.noreply.github.com>
2023-06-05 11:51:57 -0400

    rename files

--- location_service/coordinates/insert.py
+++ location_service/coordinates/insert.py
@@ -5,14 +5,14 @@ import os
 
 # load environment variables
 load_dotenv()
-DB_HOST = os.getenv('DB_HOST')
-DB_NAME = os.getenv('DB_NAME')
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-engine = create_engine(f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}')
+engine = create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}")
 
-df = pd.read_csv('mock_database/bulk_inserts/coordinates/coordinates.csv')
+df = pd.read_csv("mock_database/bulk_inserts/coordinates/coordinates.csv")
 
 with engine.connect() as connection:
     try:
@@ -21,9 +21,9 @@ with engine.connect() as connection:
                 "INSERT INTO location_service.coordinates (coordinates, latitude, longitude) VALUES (ST_GeomFromText(:coordinates, 4326), :latitude, :longitude)"
             )
             params = {
-                'coordinates': f'POINT({row.latitude} {row.longitude})',
-                'latitude': row.latitude,
-                'longitude': row.longitude
+                "coordinates": f"POINT({row.latitude} {row.longitude})",
+                "latitude": row.latitude,
+                "longitude": row.longitude,
             }
             connection.execute(stmt, params)
         connection.commit()
