407cbd8f5583cc13c49c276398bb2794ea23fea9
Rodrigo Tiscareno <rodrigotiscareno1@gmail.com>
2023-06-06 20:48:09 -0500

    added fail, replace, append options

--- mock_database/bulk_inserts/coordinates/insert.py
+++ mock_database/bulk_inserts/coordinates/insert.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 import pandas as pd
 import os
 
-# load environment variables
 load_dotenv()
 DB_HOST = os.getenv('DB_HOST')
 DB_NAME = os.getenv('DB_NAME')
@@ -14,8 +13,19 @@ engine = create_engine(f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_N
 
 df = pd.read_csv('mock_database/bulk_inserts/coordinates/coordinates.csv')
 
+# INSERT_MODE can be 'fail', 'replace', or 'append'
+INSERT_MODE = 'replace'
+
 with engine.connect() as connection:
     try:
+        if INSERT_MODE == 'fail':
+            result = connection.execute(text('SELECT COUNT(*) FROM location_service.coordinates'))
+            if result.scalar() > 0:
+                raise Exception("Data already exists in the table")
+        elif INSERT_MODE == 'replace':
+            print("Replacing the data in the database")
+            connection.execute(text('DELETE FROM location_service.coordinates'))
+        print("Inserting new data into location_service.coordinates")
         for index, row in df.iterrows():
             stmt = text(
                 "INSERT INTO location_service.coordinates (coordinates, latitude, longitude) VALUES (ST_GeomFromText(:coordinates, 4326), :latitude, :longitude)"
@@ -27,5 +37,6 @@ with engine.connect() as connection:
             }
             connection.execute(stmt, params)
         connection.commit()
+        print("Success")
     except Exception as e:
         print(f"An error occurred: {e}")
