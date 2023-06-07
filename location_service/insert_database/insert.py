0d73247f9bfc3b509689a4a99813fd3fff8a8e47
Ryan Lam <53351930+ryan-lam@users.noreply.github.com>
2023-06-07 19:39:41 -0400

    fix formatting

--- location_service/insert_database/insert.py
+++ location_service/insert_database/insert.py
@@ -4,36 +4,38 @@ import pandas as pd
 import os
 
 load_dotenv()
-DB_HOST = os.getenv('DB_HOST')
-DB_NAME = os.getenv('DB_NAME')
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-engine = create_engine(f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}')
+engine = create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}")
 
-df = pd.read_csv('location_service/insert_database/sample_coordinates.csv')
+df = pd.read_csv("location_service/insert_database/sample_coordinates.csv")
 
 # INSERT_MODE can be 'fail', 'replace', or 'append'
-INSERT_MODE = 'replace'
+INSERT_MODE = "replace"
 
 with engine.connect() as connection:
     try:
-        if INSERT_MODE == 'fail':
-            result = connection.execute(text('SELECT COUNT(*) FROM location_service.coordinates'))
+        if INSERT_MODE == "fail":
+            result = connection.execute(
+                text("SELECT COUNT(*) FROM location_service.coordinates")
+            )
             if result.scalar() > 0:
                 raise Exception("Data already exists in the table")
-        elif INSERT_MODE == 'replace':
+        elif INSERT_MODE == "replace":
             print("Replacing the data in the database")
-            connection.execute(text('DELETE FROM location_service.coordinates'))
+            connection.execute(text("DELETE FROM location_service.coordinates"))
         print("Inserting new data into location_service.coordinates")
         for index, row in df.iterrows():
             stmt = text(
                 "INSERT INTO location_service.coordinates (coordinates, latitude, longitude) VALUES (ST_GeomFromText(:coordinates, 4326), :latitude, :longitude)"
             )
             params = {
-                'coordinates': f'POINT({row.latitude} {row.longitude})',
-                'latitude': row.latitude,
-                'longitude': row.longitude
+                "coordinates": f"POINT({row.latitude} {row.longitude})",
+                "latitude": row.latitude,
+                "longitude": row.longitude,
             }
             connection.execute(stmt, params)
         connection.commit()
