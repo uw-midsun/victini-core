ccfdb5873a53b40f7ca7f1328ba9002868c4e0db
probro27 <prabhavkhera@hotmail.com>
2023-07-09 17:12:48 -0400

    Formatting fixed and documentation added

---
+++ db_gateway/README.md
@@ -0,0 +1,17 @@
+# DB Gateway
+
+## Description
+
+This is a simple gateway to a database. It is a simple REST API that allows you to perform CRUD operations on a database using GraphQL queries. It is written in Python and uses the Flask framework.
+
+## Setup
+
+- Install requirements: `pip install -r requirements.txt`
+- Run the server: `python app.py`
+
+## Usage
+
+- The server will be running on `localhost:5000`
+- To check if the database is connected, go to `localhost:5000/health`
+
+Further documentation will be added soon.
--- db_gateway/app.py
+++ db_gateway/app.py
@@ -6,38 +6,51 @@ import urllib, os
 from sqlalchemy import text
 from dotenv import load_dotenv
 import pymysql
+
 pymysql.install_as_MySQLdb()
 
 load_dotenv()
 
-DATABASE_HOST = os.getenv('DATABASE_HOST')
-DATABASE_NAME = os.getenv('DATABASE_NAME')
-DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
-DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
 DATABASE_PASSWORD_UPDATED = urllib.parse.quote_plus(DATABASE_PASSWORD)
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://'+DATABASE_USERNAME+':'+DATABASE_PASSWORD_UPDATED +'@'+DATABASE_HOST+'/'+DATABASE_NAME
-app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "mysql://"
+    + DATABASE_USERNAME
+    + ":"
+    + DATABASE_PASSWORD_UPDATED
+    + "@"
+    + DATABASE_HOST
+    + "/"
+    + DATABASE_NAME
+)
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-@app.route('/test', methods=['GET'])
-def test():
-    return jsonify({'message': 'Hello World!'})
 
-@app.route('/health', methods=['GET'])
+@app.route("/test", methods=["GET"])
+def test():
+    return jsonify({"message": "Hello World!"})
+
+
+@app.route("/health", methods=["GET"])
 def test_db():
     with app.app_context():
         try:
-            db.session.execute(text('SELECT 1'))
-            return jsonify({'message': 'Connection successful!'})
+            db.session.execute(text("SELECT 1"))
+            return jsonify({"message": "Connection successful!"})
         except Exception as e:
             print(e)
-            return jsonify({'message': 'Connection_failed!'})
+            return jsonify({"message": "Connection_failed!"})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
