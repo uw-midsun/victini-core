ff55f6d7da61d5ee520483359be121b3b9b5e28c
Ryan Lam <53351930+ryan-lam@users.noreply.github.com>
2023-07-14 16:31:34 -0400

    add geopy to find dist btwn points

--- routemodel2/gpx_to_csv.py
+++ routemodel2/gpx_to_csv.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 from bs4 import BeautifulSoup
+from geopy import distance
 
 
 def gpx_json_to_df(json_filepath):
@@ -15,18 +16,28 @@ def gpx_json_to_df(json_filepath):
             "step": point.get("step", None),
             "next_turn": point.get("nextturn", None),
             "dir": None,
-            "dist_to_next_m": None,
-            "elapsed_dist_m": None,
-            "time_to_next_s": None,
+            "gpx_dist_to_next_waypoint_m": None,
+            "gpx_elapsed_dist_m": None,
+            "geopy_elapsed_dist_m": 0,
+            "geopy_dist_from_last_m": 0,
+            # "time_to_next_s": None,
         }
         if dir := point.get("dir", None):
             soup = BeautifulSoup(dir, "html.parser")
             data["dir"] = soup.get_text()
         if dist := point.get("dist", None):
-            data["elapsed_dist_m"] = dist["total"] - dist["val"]
-            data["dist_to_next_m"] = dist["val"]
-        if time := point.get("timeto", None):
-            data["time_to_next_s"] = time["val"]
+            data["gpx_dist_to_next_waypoint_m"] = dist["val"]
+            data["gpx_elapsed_dist_m"] = dist["total"] - dist["val"]
+        if len(df_rows) > 0:
+            prev = df_rows[-1]
+            prev_coor = (prev["lat"], prev["lon"])
+            curr_coor = (data["lat"], data["lon"])
+            dist = distance.great_circle(prev_coor, curr_coor).m
+            data["geopy_dist_from_last_m"] = dist
+            data["geopy_elapsed_dist_m"] = prev["geopy_elapsed_dist_m"] + dist
+        # if time := point.get("timeto", None):
+        #     data["time_to_next_s"] = time["val"]
+
         df_rows.append(data)
     return pd.DataFrame(df_rows)
 
