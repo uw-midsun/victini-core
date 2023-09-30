c93402eefc767790918ad0c5d1c917f913d000c7
Kenneth Ruan <kennethjruan@gmail.com>
2023-09-30 15:13:36 -0400

    Update readme

--- etl/etl_routemodel/etl_routemodel.py
+++ etl/etl_routemodel/etl_routemodel.py
@@ -2,6 +2,9 @@
 etl_routemodel.py loads routemodel data into the database in two steps:
 1. Creates data/routemodel.csv from a gpx file
 2. Takes data/routemodel.csv and loads it into the Azure Postgres db
+
+Here is a suggested route: https://www.google.ca/maps/dir/Fulton,+MO,+USA/Davenport/Great+River+Rd+%26+Gaylord+Nelson+Hwy,+Trenton,+WI+54014,+USA/@42.7215958,-91.0372306,7z/data=!3m1!4b1!4m35!4m34!1m5!1m1!1s0x87dc8e579b700d25:0xf0215f3f96f20e66!2m2!1d-91.9479586!2d38.8467082!1m20!1m1!1s0x87e234c5e012a2f1:0xe8ea1f6356581fb0!2m2!1d-90.5776367!2d41.5236437!3m4!1m2!1d-92.2319476!2d44.4238558!3s0x87f8338724908edf:0xca9b25d292ef7845!3m4!1m2!1d-92.3766828!2d44.5058247!3s0x87f82f351959945f:0x50b0245a3e723deb!3m4!1m2!1d-92.4467661!2d44.544585!3s0x87f827936d27e451:0xb48e8cc83edcbde7!1m5!1m1!1s0x87f78be36d3d1e0b:0x177144dc524ef09d!2m2!1d-92.5279297!2d44.6013121!3e0?entry=ttu
+Route is 535 miles along the Mississippi River from Fulton, MO to Trenton, WI :)
 """
 
 # Step 1
@@ -15,8 +18,6 @@ from geopy import distance
 import sys, os
 from os.path import dirname, abspath, join, normpath
 sys.path.append(normpath(abspath(join(dirname(__file__), '..', '..'))))
-
-print(sys.path)
 
 def gpx_json_to_df(json_filepath):
     df_rows = []
@@ -34,6 +35,7 @@ def gpx_json_to_df(json_filepath):
             "gpx_elapsed_dist_m": None,
             "geopy_elapsed_dist_m": 0,
             "geopy_dist_from_last_m": 0,
+            "weather_id": None,
             # "time_to_next_s": None,
         }
         if dir := point.get("dir", None):
@@ -66,9 +68,9 @@ def check_null(value):
 
 def seed_from_csv(filename):
     # import all modules here that might define models before calling init_db()
-    from db_gateway.src.models import RouteModel
+    from db_gateway.src.models import RouteModel, Weather
 
-    Base.metadata.create_all(bind=engine, tables=[RouteModel.__table__])
+    Base.metadata.create_all(bind=engine, tables=[RouteModel.__table__, Weather.__table__])
 
     df = pd.read_csv(filename)
     for row in df.itertuples():
@@ -84,6 +86,7 @@ def seed_from_csv(filename):
             gpx_elapsed_dist_m=check_null(row.gpx_elapsed_dist_m),
             geopy_elapsed_dist_m=check_null(row.geopy_elapsed_dist_m),
             geopy_dist_from_last_m=check_null(row.geopy_dist_from_last_m),
+            weather_id=None,
         )
         db_session.add(location)
     db_session.commit()
