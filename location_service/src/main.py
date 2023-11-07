29122dd46795e550fe24ac957553d463aba490d8
Ryan Lam <53351930+ryan-lam@users.noreply.github.com>
2023-11-07 17:25:42 -0500

    have location service send routemodel id to mongo

--- location_service/src/main.py
+++ location_service/src/main.py
@@ -1,10 +1,14 @@
 import json
 import os
+import time
 
+import pymongo
 from dotenv import load_dotenv
 from flask import Flask, request
 from psycopg2 import pool as psycopg2_pool
 
+
+# Env vars
 load_dotenv()
 REQUIRE_AUTH = os.getenv("REQUIRE_AUTH", "False").lower() in ("true", "1", "t")
 AUTH_KEY = os.environ.get("AUTH_KEY")
@@ -13,8 +17,12 @@ DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
+MONGODB_CONNECTION_STRING = os.environ.get("MONGODB_CONNECTION_STRING")
+MONGODB_NAME = os.environ.get("MONGODB_NAME")
+MONGODB_COLLECTION = os.environ.get("MONGODB_COLLECTION")
 
 
+# Create dbv connections
 app = Flask(__name__)
 postgres_pool = psycopg2_pool.SimpleConnectionPool(
     minconn=1,
@@ -25,6 +33,9 @@ postgres_pool = psycopg2_pool.SimpleConnectionPool(
     user=DB_USER,
     password=DB_PASSWORD,
 )
+client = pymongo.MongoClient(MONGODB_CONNECTION_STRING)
+db = client[MONGODB_NAME]
+collection = db[MONGODB_COLLECTION]
 
 
 def authorized(auth_key):
@@ -66,18 +77,36 @@ def closest_location():
     if lat is None or lon is None:
         return "Invalid request body", 400
 
-    query = f"""
+    query_location = f"""
     SELECT *
     FROM location_service 
     ORDER BY location_service.geo <-> ST_SetSRID(ST_MakePoint({lon}, {lat}), 4326)
     LIMIT 1;
     """
+    insert_location_logs = f"""
+    INSERT INTO table_name (column1, column2, column3, ...)
+    VALUES (value1, value2, value3, ...); 
+    """
     conn = postgres_pool.getconn()
     cur = conn.cursor()
-    cur.execute(query)
+    cur.execute(query_location)
     location = dict(zip(("id", "lat", "lon"), cur.fetchone()))
     cur.close()
     postgres_pool.putconn(conn)
+
+    res = collection.update_one(
+        {"config_type": "location_service"},
+        {
+            "$set": {
+                "routemodel_id": location["id"],
+                "location_service_log_id": 0,
+                "updated_at": int(time.time()),
+                "active": True,
+            }
+        },
+        upsert=True,
+    )
+    print(res)
     return location
 
 
